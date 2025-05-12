dbcb249ad32675621a1922b091afbd075aad29c9
Kunal Salvi <kunalsalvi63@gmail.com>
2025-05-12 18:51:20 -0500

    first commit

---
+++ Debug/Driver/Timer/Timer.d
@@ -0,0 +1,24 @@
+Driver/Timer/Timer.o: ../Driver/Timer/Timer.c ../Driver/Timer/Timer.h \
+ ../Inc/main.h ../Inc/stm32f407xx.h ../Inc/core_cm4.h \
+ ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h \
+ ../Inc/mpu_armv7.h ../Inc/system_stm32f4xx.h ../Inc/stm32f4xx.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO_Defs.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA_Defs.h \
+ ../Driver/Timer/Timer_Defs.h
+../Driver/Timer/Timer.h:
+../Inc/main.h:
+../Inc/stm32f407xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
+../Inc/stm32f4xx.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO_Defs.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA_Defs.h:
+../Driver/Timer/Timer_Defs.h:
---
+++ Release/Src/main.d
@@ -0,0 +1,25 @@
+Src/main.o: ../Src/main.c ../Inc/main.h ../Inc/stm32f407xx.h \
+ ../Inc/core_cm4.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
+ ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h ../Inc/system_stm32f4xx.h \
+ ../Inc/stm32f4xx.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/Timer/Timer.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO_Defs.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA_Defs.h \
+ D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/Timer/Timer_Defs.h
+../Inc/main.h:
+../Inc/stm32f407xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
+../Inc/stm32f4xx.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/Timer/Timer.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/GPIO/GPIO_Defs.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/DMA/DMA_Defs.h:
+D:/STM32F407_Projects/Seven_Segment_Display_Driver/Driver/Timer/Timer_Defs.h:
